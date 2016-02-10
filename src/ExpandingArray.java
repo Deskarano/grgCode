44434a9cd4ebc6a64f62cd843fbc5a7bfd5209dc
Deskarano <gregorhaas1997@gmail.com>
2016-02-09 22:03:19 -0500

    Update ExpandingArray.java

--- src/ExpandingArray.java
+++ src/ExpandingArray.java
@@ -7,103 +7,103 @@ public class ExpandingArray
 
     public ExpandingArray()
     {
-	data = new Object[0];
-	length = 0;
+        data = new Object[0];
+        length = 0;
     }
-	
+
     public void add(Object newObject)
     {
-	expand();
-	
-	data[length - 1] = newObject;
+        expand();
+
+        data[length - 1] = newObject;
     }
 
     public void deleteIndex(int index)
     {
-	Object[] tempData = new Object[length - 1];
-	
-	int arrayIndex = 0;
-	
-	for(int i = 0; i < index; i++)
-	{
-	    tempData[arrayIndex] = data[i];
-	    arrayIndex++;
-	}
-	
-	for(int i = index + 1; i < length; i++)
-	{
-	    tempData[arrayIndex] = data[i];
-	    arrayIndex++;
-	}
-	
-	length--;
-	data = tempData;
+        Object[] tempData = new Object[length - 1];
+
+        int arrayIndex = 0;
+
+        for (int i = 0; i < index; i++)
+        {
+            tempData[arrayIndex] = data[i];
+            arrayIndex++;
+        }
+
+        for (int i = index + 1; i < length; i++)
+        {
+            tempData[arrayIndex] = data[i];
+            arrayIndex++;
+        }
+
+        length--;
+        data = tempData;
     }
 
     public void deleteObject(Object obj)
     {
-	int index = -1;
-	
-	for(int i = 0; i < length; i++)
-	{
-	    if(data[i].equals(obj))
-	    {
-		index = i;
-	    }
-	}
-	
-	if(index != -1)
-	{
-	    deleteIndex(index);
-	}
+        int index = -1;
+
+        for (int i = 0; i < length; i++)
+        {
+            if (data[i].equals(obj))
+            {
+                index = i;
+            }
+        }
+
+        if (index != -1)
+        {
+            deleteIndex(index);
+        }
     }
-    
+
     public Object get(int index)
     {
-	return data[index];
+        return data[index];
     }
 
     public void set(int index, Object obj)
     {
-	data[index] = obj;
+        data[index] = obj;
     }
-    
+
     public boolean isPresent(Object obj)
     {
-	for(int i = 0; i < length; i++)
-	{
-	    if(data[i].equals(obj))
-	    {
-		return true;
-	    }
-	}
-	
-	return false;
+        for (int i = 0; i < length; i++)
+        {
+            if (data[i].equals(obj))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
-    
+
     public int getIndex(Object obj)
     {
-	for(int i = 0; i < length; i++)
-	{
-	    if(data[i].equals(obj))
-	    {
-		return i;
-	    }
-	}
-	
-	return -1;
+        for (int i = 0; i < length; i++)
+        {
+            if (data[i].equals(obj))
+            {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     private void expand()
     {
-	Object[] tempData = new Object[length + 1];
-	
-	for(int i = 0; i < length; i++)
-	{
-	    tempData[i] = data[i];
-	}
-	
-	length++;
-	data = tempData;
+        Object[] tempData = new Object[length + 1];
+
+        for (int i = 0; i < length; i++)
+        {
+            tempData[i] = data[i];
+        }
+
+        length++;
+        data = tempData;
     }
 }
